8f1c0cf0fd36e5fbb036d5e2d0f157d1672c22fb
Anthony Cortese <akcortese@Anthonys-MacBook-Pro.local>
2022-02-23 20:28:34 -0700

    created functions for updating and editing reviews, and added them into the review reducer

--- src/components/review/reviewReducer.ts
+++ src/components/review/reviewReducer.ts
@@ -1,50 +1,97 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "../../redux/store";
 
 import Review from "../../types/Review";
 
 import { AppThunk } from "../../redux/store";
-import { fetchReviews } from "../../services/reviews";
+import { fetchReviews,
+    postReview,
+    patchReview,
+    deleteReview } from "../../services/reviews";
 
-export interface ReviewState {
-    reviews: Review[] | undefined
-}
-
-const initialState: ReviewState = {
-    reviews: undefined,
-
-}
-
-export const loadReviews = createAsyncThunk(
-    'indexReviews/getStatus',
-    async () => {
-        const response = await fetchReviews()
-        return response
+    export interface ReviewState {
+        reviews: Review[] | undefined
+        currentEdit: Review | undefined
     }
-)
-
-export const reviewSlice = createSlice({
-    name: 'review',
-    initialState,
-    reducers: {},
-    extraReducers: builder => {
-        builder
-            .addCase(loadReviews.fulfilled, (state, action) => {
-                state.reviews = action.payload
-            })
-        }   
-
-})
-
-export const selectReviews = (state: RootState) => state.review.reviews
-
-export const getReviewInfo =
-    (searchId: string): AppThunk =>
-    (dispatch, getState) => {
-        const currentValue = selectReviews(getState())
-        const result = currentValue?.find(({ id }) => id === searchId)
-        if (!result) throw new Error('No review was found')
+    
+    const initialState: ReviewState = {
+        reviews: undefined,
+        currentEdit: undefined
     }
-
-
-export default reviewSlice.reducer
+    
+    export const loadReviews = createAsyncThunk(
+        'indexReviews/getStatus',
+        async () => {
+            const response = await fetchReviews()
+            return response
+        }
+    )
+    
+    export const addReview = createAsyncThunk(
+        'postReviews/getStatus',
+        async (review: Review) => {
+            const response = await postReview(review)
+            return response
+        }
+    )
+    
+    export const updateReview = createAsyncThunk(
+        'patchReview/getStatus',
+        async (review: Review) => {
+            const response = await patchReview(review)
+            return response
+        }
+    )
+    
+    export const removeReview = createAsyncThunk(
+        'deleteReviews/getStatus',
+        async (id: string) => {
+            const response = await deleteReview(id)
+            return response
+        }
+    )
+    
+    export const reviewSlice = createSlice({
+        name: 'review',
+        initialState,
+        reducers: {
+            setCurrentEdit: (state, action: PayloadAction<Review>) => {
+                state.currentEdit = action.payload
+            },
+            resetCurrentEdit: state => {
+                state.currentEdit = undefined
+            }
+        },
+        extraReducers: builder => {
+            builder
+                .addCase(loadReviews.fulfilled, (state, action) => {
+                    state.reviews = action.payload
+                })
+                .addCase(addReview.fulfilled, (state, action) => {
+                    state.reviews = action.payload
+                })
+                .addCase(updateReview.fulfilled, (state, action) => {
+                    state.reviews = action.payload
+                })
+                .addCase(removeReview.fulfilled, (state, action) => {
+                    state.reviews = action.payload
+                })
+        }
+    })
+    
+    export const { setCurrentEdit, resetCurrentEdit } = reviewSlice.actions
+    
+    export const selectReviews = (state: RootState) => state.review.reviews
+    
+    export const selectCurrentEdit = (state: RootState) => state.review.currentEdit
+    
+    export const getReviewInfo =
+        (searchId: string): AppThunk =>
+        (dispatch, getState) => {
+            const currentValue = selectReviews(getState())
+            const result = currentValue?.find(({ id }) => id === searchId)
+            if (!result) throw new Error('No review was found')
+            dispatch(setCurrentEdit(result))
+        }
+    
+    export default reviewSlice.reducer
