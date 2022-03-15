b7b8adc916ab0fad18b5ea64d237f17ba3be375f
Anthony Cortese <akcortese@Anthonys-MacBook-Pro.local>
2022-03-15 06:44:49 -0600

    working on the ratings system and radio buttons

--- src/components/review/ReviewForm.tsx
+++ src/components/review/ReviewForm.tsx
@@ -5,6 +5,7 @@ import Button from '../ui/Button'
 import Review from '../../types/Review'
 
 import s from './ReviewForm.module.css'
+import Rating from '../ui/Rating'
 
 interface IReviewForm {
     defaultValues: Review
@@ -40,14 +41,47 @@ export default function ReviewForm({
                     </p>
                 )}
             </div>
-            <div>
+            Rating
+            <div className={s.rating}>
+                    <Rating />
+                
                 <label htmlFor="genres">
-                    Genres
-                    <input
-                        type="text"
-                        placeholder="Movie Genres"
+                    <input type="radio" value="5" id="5"
+    
                         {...register('genres', { required: true })}
                     />
+                   ☆
+                </label>
+                
+                <label htmlFor="genres">
+                    <input type="radio" value="4" id="4"
+    
+                        {...register('genres', { required: true })}
+                    />
+                    ☆
+                </label>
+                <label htmlFor="genres">
+                    <input type="radio" value="3" id="3"
+    
+                        {...register('genres', { required: true })}
+                    />
+                    ☆
+                </label>
+
+                <label htmlFor="genres">
+                    <input type="radio" value="2" id="2"
+    
+                        {...register('genres', { required: true })}
+                    />
+                    ☆
+                </label>
+            
+                <label htmlFor="genres">
+                    <input type="radio" value="1" id="1"
+    
+                        {...register('genres', { required: true })}
+                    />
+                    ☆
                 </label>
                 {errors.genres && (
                     <p className={s.error} data-testid="error_message">
---
+++ src/components/ui/Rating.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import s from './Rating.module.css'
+
+function Rating() {
+  return (
+    
+<div className={s.starrating}>
+
+  
+<div className={s.bd}>
+   <input type="radio" name="stars" id="star-6"/>
+   <label htmlFor="star-6"></label>
+
+   <input type="radio" name="stars" id="star-5"/>
+   <label htmlFor="star-5"></label>
+
+   <input type="radio" name="stars" id="star-4"/>
+   <label htmlFor="star-4"></label>
+
+   <input type="radio" name="stars" id="star-3"/>
+   <label htmlFor="star-3"></label>
+
+   <input type="radio" name="stars" id="star-2"/>
+   <label htmlFor="star-2"></label>
+   
+   <input type="radio" name="stars" id="star-1"/>
+   <label htmlFor="star-1"></label>
+</div>
+</div>
+  )
+}
+
+export default Rating
